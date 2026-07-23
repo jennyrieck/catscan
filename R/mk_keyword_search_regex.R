635425a48fe5159ef8335a5a7a580af069b1c89e
jrieck <jennifer.rieck@hc-sc.gc.ca>
2026-07-23 09:24:58 -0400

    docs: fix example

--- R/mk_keyword_search_regex.R
+++ R/mk_keyword_search_regex.R
@@ -1,7 +1,7 @@
 #' Turn search terms into regex
 #'
 #' Takes a string vector and converts it to a dataframe with regex formatting
-#' to handle standalone strings, case sensitivity, and basic pluralization.
+#' to handle word boundaries, case sensitivity, and basic pluralization.
 #' Creates a dataframe compatible for use with [keyword_search()]
 #'
 #' @param KEYWORDS a string vector of search terms
@@ -9,8 +9,8 @@
 #' @param IGNORE_CASE boolean; ignore upper/lower case? default = TRUE
 #' @param MAKE_PLURAL boolean; make keyword plural by adding "s" to the end? default = FALSE
 #' @examples
-#' KEYWORDS_DF <- mk_keyword_search_keycode(c("courriel", "e[-]?mail"),
-#'                                 STANDALONE = TRUE, IGNORE_CASE = TRUE, MAKE_PLURAL = TRUE)
+#' KEYWORDS_DF <- mk_keyword_search_regex(c("Pixel", "Beach(es)?"),
+#'                                 STANDALONE = TRUE, IGNORE_CASE = TRUE, MAKE_PLURAL = FALSE)
 #'
 #' @returns dataframe of keywords and the corresponding regex keycodes
 #' @export
