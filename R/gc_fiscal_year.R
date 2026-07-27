6d5b9baff2d8834566f0480331432aee1d5be356
jrieck <jennifer.rieck@hc-sc.gc.ca>
2026-07-27 10:10:47 -0400

    feat: importFrom lubridate as example for documentation

--- R/gc_fiscal_year.R
+++ R/gc_fiscal_year.R
@@ -17,6 +17,8 @@
 #' gc_fiscal_year(as.Date("2026-03-31"), fy_format = "long")
 #' gc_fiscal_year(as.Date(c("2026-01-01", "2026-04-20")), fy_format = "short")
 #'
+#' @importFrom lubridate year month
+#'
 #' @export
 #'
 gc_fiscal_year <- function(date, fy_format = c("long", "short")) {
@@ -31,11 +33,11 @@ gc_fiscal_year <- function(date, fy_format = c("long", "short")) {
   }
 
   # Extract Year and Month from date
-  year <- as.integer(format(date, "%Y"))
-  month <- as.integer(format(date, "%m"))
+  date_year <- lubridate::year(date)
+  date_month <- lubridate::month(date)
 
   # Determine starting year based on month
-  start_year <- ifelse(month >= 4, year, year - 1)
+  start_year <- ifelse(date_month >= 4, date_year, date_year - 1)
   end_year <- start_year + 1
 
   # Format fiscal year output
